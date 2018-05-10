fc06a6353f8df1abf5b0bc4c89110dc8e8ea3708
gazzza39 <gazzza39@hotmail.co.uk>
2018-05-10 12:44:42 +0000

    added system users module and removed gitignore

---
+++ modules/system_users/manifests/admins.pp
@@ -0,0 +1,21 @@
+Class system_users::admins {
+ package { 'csh':
+  ensure => latest,
+}
+
+ group { 'staff':
+  ensure => present,
+}
+
+ user { 'admin':
+  group => 'staff',
+  shell => '/bin/csh',
+  require => Package ['csh'],
+ } 
+
+}
+
+
+
+
+
